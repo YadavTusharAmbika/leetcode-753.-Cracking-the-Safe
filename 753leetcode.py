140963f33d20ba3ddcf355eeb709f4f0607e580b
Yadav Tushar Ambika <88570217+YadavTusharAmbika@users.noreply.github.com>
2026-01-06 19:56:20 +0530

    Create 753leetcode.py

---
+++ 753leetcode.py
@@ -0,0 +1,21 @@
+class Solution:
+    def crackSafe(self, n, k):
+        
+        seen = set()
+        res = []
+        start = "0" * (n - 1) 
+
+        def dfs(node):
+            for i in range(k):
+                edge = node + str(i)
+                if edge not in seen:
+                    seen.add(edge)
+                    dfs(edge[1:])
+                    res.append(str(i))
+
+        dfs(start)
+        return start + "".join(res[::-1])
+
+
+
+ // explaination is written in readme file //
